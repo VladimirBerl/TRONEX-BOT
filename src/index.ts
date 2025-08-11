7146b4d34edde3a5b72e4dc9f1f3da66a4ee3abb
VladimirBerl <vova2002.13lol@gmail.com>
2025-08-11 23:42:01 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,46 +2,12 @@ import { Telegraf } from 'telegraf';
 import { BOT_TOKEN } from './const.js';
 
 import { setupAdminCommands } from './commands/admin/index.js';
-import { apiReferral } from './api/admin/referrals.js';
-import { apiAuth } from './api/users/auth.js';
+import { setupReferralCommands } from './commands/user/referral.js';
 
 if (!BOT_TOKEN) throw new Error('BOT_TOKEN is missing in environment');
 const bot = new Telegraf(BOT_TOKEN);
 
-bot.start(async (ctx) => {
-  const payload = ctx.payload;
-  const id = ctx.from.id;
-  const username = ctx.from.username ?? ctx.from.last_name ?? ctx.from.first_name;
-
-  try {
-    await apiAuth.auth(String(id), username);
-  } catch (error) {
-    console.error(error);
-    return ctx.reply('При авторизации произошла ошибка, попробуйте еще раз.');
-  }
-
-  if (!payload || !payload.startsWith('invited_by_')) {
-    return ctx.reply('Добро пожаловать!');
-  }
-
-  const inviter_id_tg = payload.replace('invited_by_', '');
-
-  try {
-    const res = await apiReferral.addReferral(inviter_id_tg, String(id));
-
-    if (res?.created) {
-      await ctx.reply('Добро пожаловать! Вы были успешно зарегистрированы как реферал!');
-    } else {
-      await ctx.reply('Добро пожаловать! Вы уже были приглашены ранее.');
-    }
-  } catch (err) {
-    console.error('Ошибка при создании реферала:', err);
-    await ctx.reply(
-      'Приглашающего пользователя не существует! Реферальная система не прошла, попробуйте еще раз.'
-    );
-  }
-});
-
+setupReferralCommands(bot);
 setupAdminCommands(bot);
 
 bot
